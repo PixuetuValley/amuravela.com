295155a78021c71c905591851d3f76c435a3a6b0
Asur <asur@asurbernardo.com>
2020-11-28 18:49:37 +0100

    Add PostCSS support and start organization and styling

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+const themeDir = __dirname + '/assets/css';
+
+module.exports = {
+    plugins: {
+        'postcss-import': {
+            path: [themeDir]
+        },
+        'postcss-nested': {},
+        'postcss-combine-media-query': {},
+        'postcss-combine-duplicated-selectors': {},
+        'autoprefixer': {
+            browsers: ['>1%']
+        },
+        '@fullhuman/postcss-purgecss':{
+            content: [
+                './layouts/**/*.html'
+            ]
+        }
+    }
+}
